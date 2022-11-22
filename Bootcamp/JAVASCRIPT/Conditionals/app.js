f4dffd0aef2b77ad2842410e6cbff0361f57a110
Cem Olmez <c.olmez123@gmail.com>
2022-11-22 15:05:46 +0300

    JAVA 22.11.2022

---
+++ Bootcamp/JAVASCRIPT/Conditionals/app.js
@@ -0,0 +1,38 @@
+// let random = Math.random()
+// if ( random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+//     console.log(random);
+// }
+
+// let random = Math.random()
+// if ( random >= 0.5) {
+//     console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
+//     console.log(random);
+// }
+
+
+
+
+// const dayOfWeek = "Friday";
+
+// if (dayOfWeek === "Monday") {
+//     console.log("UGHHH I HATE MONDAYS!")
+// } 
+// else if (dayOfWeek === "Saturday") {
+//     console.log("YAY I LOVE SATURDAYS!")
+// }
+// else if (dayOfWeek === "Friday") {
+//     console.log("FRIDAY ARE DECENT,ESPECIALLY AFTER WORK")
+// }
+
+
+
+
+// 0-5 -FREE
+// 5-10 CHILD $10
+// 10-65 ADULT $20
+// 65+ SENIOR $10
+
+const age = 8
+
+if (age)
