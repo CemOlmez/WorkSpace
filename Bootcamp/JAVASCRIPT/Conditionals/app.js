650ccd3b9e0ef461300b471ebda19f2a02177a7e
Cem Olmez <c.olmez123@gmail.com>
2022-11-23 18:21:57 +0300

    JAVA 23.11.2022

--- Bootcamp/JAVASCRIPT/Conditionals/app.js
+++ Bootcamp/JAVASCRIPT/Conditionals/app.js
@@ -1,28 +1,28 @@
-// let random = Math.random()
-// if ( random < 0.5) {
-//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
-//     console.log(random);
-// }
-
-// let random = Math.random()
-// if ( random >= 0.5) {
-//     console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
-//     console.log(random);
-// }
+let random = Math.random()
+if ( random < 0.5) {
+    console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+}
+else {
+    console.log("YOUR NUMBER IS GREATER THAN 0.5!!!");
+}
+console.log(random);
 
 
 
 
-// const dayOfWeek = "Friday";
+// const dayOfWeek = prompt("ENTER A DAY").toLowerCase();
 
-// if (dayOfWeek === "Monday") {
+// if (dayOfWeek === "monday") {
 //     console.log("UGHHH I HATE MONDAYS!")
 // } 
-// else if (dayOfWeek === "Saturday") {
+// else if (dayOfWeek === "saturday") {
 //     console.log("YAY I LOVE SATURDAYS!")
 // }
-// else if (dayOfWeek === "Friday") {
+// else if (dayOfWeek === "friday") {
 //     console.log("FRIDAY ARE DECENT,ESPECIALLY AFTER WORK")
+// }
+// else {
+//     console.log("MEH")
 // }
 
 
@@ -33,6 +33,15 @@
 // 10-65 ADULT $20
 // 65+ SENIOR $10
 
-const age = 8
+// const age = 8
 
-if (age)
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free!")
+// }
+// else if (age < 10) {
+//     console.log("You are a child. You pay $10")
+// }
+// else if (age < 65) {
+//     console.log("You are an adult. You pay $20")
+// }
+
