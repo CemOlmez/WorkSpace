07ddd019d123c321cb919fc885df0ee3c1af1e27
Cem Olmez <c.olmez123@gmail.com>
2023-01-02 17:47:16 +0300

    Update V02.01.2023 todo list

--- Bootcamp/JAVASCRIPT/LOOPS/TO_DO_LIST/app.js
+++ Bootcamp/JAVASCRIPT/LOOPS/TO_DO_LIST/app.js
@@ -1,5 +1,21 @@
-let input = prompt("what would you like to do?")
+let input = prompt("what would you like to do?");
+const todos = ['collect Data', 'Clean Data'];
 while(input !== "quit" && input !== "q"){
+    if(input === "list") {
+        console.log("*************")
+        for( let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log("*************")
+    } else if (input === "new") {
+        const newTodo = prompt("Ok, what is the new todo?")
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list`)
+    } else if (input === "delete"){
+        const index = prompt("Ok, enter an index to delete:");
+        const index = parseInt(indexStr);
+        const deleted = todos
+    }
     input = prompt("what would you like to do?")
 }
 console.log("OK, YOU QUIT THE APP!")
