4112138bbc5864062fce696af5a9e524ac91eb63
CemOlmez <c.olmez123@gmail.com>
2023-12-28 19:34:54 +0300

    update

--- eletrolyte/app.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".toggle-links");
-    const icon = document.querySelector(".toggle-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
